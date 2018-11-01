26ef975175ec791d635c915d253ddc492463e230
Dimitar Petrov <dimitar.pe@sbtech.com>
2018-11-01 18:41:00 +0200

    initial commit

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            DateTime now = DateTime.Now;
+            ViewBag.now = now;
+            ViewBag.greeting = (now.Hour - 12 <= 0) ? "Good morning fucker" : "Good afternoon asshat";
+            return View("Example");
+        }
+        public IActionResult RsvpForm()
+        {
+            return View();
+        }
+    }
+}
