42d5b9d67b22887e9ffbaf859f9903beb6402d5b
Dimitar Petrov <dimitar.pe@sbtech.com>
2018-11-02 15:22:38 +0200

    chapter 3

---
+++ LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+
+        public static Product[] Get()
+        {
+            return new Product[] { new Product { Name = "crack", Price = 20.0M }, new Product { Name = "whore", Price = 200.53M } };
+        }
+    }
+}
