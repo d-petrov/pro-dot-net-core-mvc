27d1433b15b8c3cf55c33712e3fa98344a2bd4f1
Dimitar Petrov <dimitar.pe@sbtech.com>
2018-11-07 13:56:49 +0200

    sports store - initial commit - page 207

---
+++ SportsStore/SportsStore/Models/Fakes/FakeProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models;
+
+namespace SportsStore.Models.Fakes
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>()
+        {
+            new Product{Name="Butt",Price=10M},
+            new Product{Name="Tits",Price=222M}
+        }.AsQueryable<Product>();
+    }
+}
