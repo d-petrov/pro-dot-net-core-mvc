f5c062bb9d4ca69ae49e75a88f6e5f13dfd4769a
Dimitar Petrov <dimitar.pe@sbtech.com>
2018-11-20 18:45:16 +0200

    cart controller, view and extensions

---
+++ SportsStore/SportsStore/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddToCart(CartLine cartLine)
+        {
+            return View();
+        }
+    }
+}
---
+++ SportsStore/SportsStore/Infrastructure/UrlExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+namespace SportsStore.Infrastructure
+{
+    public static class UrlExtensions
+    {
+        public static string PathAndQuery(this HttpRequest http) => http.QueryString.HasValue ?
+                string.Concat(http.Path, http.QueryString) : http.Path.ToString();
+    }
+}
