74a641bab4d67fa7ccf5cbe2387d19ae89cc9cdb
Dimitar Petrov <dimitar.pe@sbtech.com>
2018-11-20 08:58:28 +0200

    added left navigation infra

---
+++ SportsStore/SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models;
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IProductRepository repo;
+
+        public NavigationMenuViewComponent(IProductRepository repo)
+        {
+            this.repo = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.SelectedCategory = RouteData?.Values["category"];
+            return View(repo.Products.Select(p => p.Category).Distinct().OrderBy(p => p));
+        }
+    }
+}
