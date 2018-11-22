722ff78d8d39f89a559f5a4d1d65ddba6d343966
Dimitar Petrov <dimitar.pe@sbtech.com>
2018-11-22 14:42:46 +0200

    summary view component and order controller

---
+++ SportsStore/SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Models;
+
+namespace SportsStore.Views.Shared.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+        //from registered service
+        public CartSummaryViewComponent(Cart cartService) => cart = cartService;
+        public IViewComponentResult Invoke() => View(cart);
+    }
+}
---
+++ SportsStore/SportsStore/Models/Order.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+
+        [BindNever]
+        IEnumerable<CartLine> Lines { get; set; }
+
+        [Required(ErrorMessage = "Address!")]
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+
+        [Required(ErrorMessage = "Name!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Country!")]
+        public string Country { get; set; }
+
+        [Required(ErrorMessage = "City!")]
+        public string City { get; set; }
+
+        public string ZipCode { get; set; }
+
+        public bool Wrap { get; set; }
+    }
+}
